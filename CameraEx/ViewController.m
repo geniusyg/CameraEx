616a69e450dee17b1f2432a65502f6062b9ff43b
Chae Yungi <l8673901@gmail.com>
2014-01-16 14:06:23 +0900

    CameraEx
    CameraEx

--- CameraEx/ViewController.m
+++ CameraEx/ViewController.m
@@ -8,11 +8,42 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imgv;
 
 @end
 
 @implementation ViewController
+- (IBAction)takePicture:(id)sender {
+	if(![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"오류" message:@"카메라 안됨" delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil];
+		[alert show];
+		return;
+	}
+	
+	UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+	ipc.delegate = self;
+	ipc.allowsEditing = YES;
+	ipc.sourceType = UIImagePickerControllerSourceTypeCamera;
+	[self presentModalViewController:ipc animated:YES];
+}
+- (IBAction)getImage:(id)sender {
+	UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+	ipc.delegate = self;
+	ipc.allowsEditing = YES;
+	ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+	[self presentModalViewController:ipc animated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+	UIImage *edited = [info objectForKey:UIImagePickerControllerEditedImage];
+	UIImage *original = [info objectForKey:UIImagePickerControllerOriginalImage];
+	
+	UIImage *using = (nil == edited) ? original : edited;
+	self.imgv.image = using;
+	
+	[picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 - (void)viewDidLoad
 {
